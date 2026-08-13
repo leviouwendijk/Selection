f11816cbe5c82e2502c6b0280297e3c733957ee3
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2026-08-13 16:20:26 +0200

    raise macOS deployment target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Selection",
+    platforms: [
+        .macOS(.v13)
+    ],
     products: [
         .library(
             name: "Selection",
